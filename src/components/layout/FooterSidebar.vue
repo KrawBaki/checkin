cd1a2fdec83dcd55bad8e5536a389d289f74c183
= <stx36@mail.ru>
2022-07-07 17:32:24 -0300

    test

--- src/components/layout/FooterSidebar.vue
+++ src/components/layout/FooterSidebar.vue
@@ -6,13 +6,13 @@
         :class="{
           'bg-[#6197FF] !w-max  rounded-full': isActive(sidebar),
         }"
-        class="flex px-4 w-[58px] duration-100 py-2 items-center"
+        class="flex px-4 w-[58px] duration-800 py-2 items-center"
         :to="sidebar.link"
         :key="sidebar.url"
       >
         <img class="mr-[8px]" :src="isActive(sidebar) ? sidebar.urlSelect : sidebar.url" alt="" />
         <p
-          class="text-[14px] whitespace-nowrap duration-1000 overflow-hidden transition-all w-min text-white"
+          class="text-[14px] whitespace-nowrap duration-700 overflow-hidden transition-all w-min text-white"
           :class="{ 'w-min text-[14px] ': isActive(sidebar) }"
         >
           {{ sidebar.title }}
